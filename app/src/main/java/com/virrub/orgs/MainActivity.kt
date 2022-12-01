926bcbd60a7d97a713b3bfb147a4104f20f23953
Virgilius santos <uniq.nestea@gmail.com>
2022-12-01 06:42:07 -0300

    add toast to main

--- app/src/main/java/com/virrub/orgs/MainActivity.kt
+++ app/src/main/java/com/virrub/orgs/MainActivity.kt
@@ -2,12 +2,13 @@ package com.virrub.orgs
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
+        Toast.makeText(this, "Hello world!!!", Toast.LENGTH_SHORT).show()
     }
 }
