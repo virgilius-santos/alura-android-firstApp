c6d186457227fff31ed3e0b1d3b9d883a25003cc
virgilius-santos <virgilius_santos@yahoo.com.br>
2022-12-11 18:19:16 -0300

    update init

--- app/src/main/java/com/virrub/orgs/productList/ProductListActivity.kt
+++ app/src/main/java/com/virrub/orgs/productList/ProductListActivity.kt
@@ -6,13 +6,12 @@ import androidx.recyclerview.widget.RecyclerView
 import com.virrub.orgs.R
 import java.math.BigDecimal
 
-class ProductListActivity : AppCompatActivity() {
+class ProductListActivity : AppCompatActivity(R.layout.activity_product_list) {
 
     private val productListRecyclerView: RecyclerView by lazy { findViewById(R.id.productList) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_product_list)
 
         productListRecyclerView.adapter = ProductListAdapter(this, listOf(
             Product(name = "name 1", description =  "description", value = BigDecimal("19.99")),
