afa1014eee7320ff356c51b56cdb0227d26e7223
virgilius-santos <virgilius_santos@yahoo.com.br>
2022-12-11 18:46:44 -0300

    create product form

---
+++ app/src/main/java/com/virrub/orgs/productForm/ProductFormActivity.kt
@@ -0,0 +1,12 @@
+package com.virrub.orgs.productForm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.virrub.orgs.R
+
+class ProductFormActivity : AppCompatActivity(R.layout.activity_product_form) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
