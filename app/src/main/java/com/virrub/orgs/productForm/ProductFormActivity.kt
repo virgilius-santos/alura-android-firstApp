a36571c04b7c75820a2eff26a534bfb6027908bf
virgilius-santos <virgilius_santos@yahoo.com.br>
2022-12-17 16:12:08 -0300

    add dialog

--- app/src/main/java/com/virrub/orgs/productForm/ProductFormActivity.kt
+++ app/src/main/java/com/virrub/orgs/productForm/ProductFormActivity.kt
@@ -1,9 +1,12 @@
 package com.virrub.orgs.productForm
 
+import android.content.DialogInterface
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputLayout
 import com.virrub.orgs.ProductsDAO
@@ -23,6 +26,7 @@ class ProductFormActivity : AppCompatActivity(R.layout.activity_product_form) {
     private val descriptionEditText: EditText by lazy { binding.edtDescription }
     private val valueEditText: EditText by lazy { binding.edtValue }
     private val saveButton: Button by lazy { binding.formBtnSave }
+    private val imageView: ImageView by lazy { binding.formImgProduct }
 
     val nameText: String
         get() { return nameEditText.text.toString() }
@@ -41,6 +45,22 @@ class ProductFormActivity : AppCompatActivity(R.layout.activity_product_form) {
             createProduct()
             finish()
         }
+
+        imageView.setOnClickListener {
+            showImageSelector()
+        }
+    }
+
+    private fun showImageSelector() {
+        AlertDialog.Builder(this)
+            .setView(R.layout.image_form)
+            .setPositiveButton("confirmar") { _, _ ->
+
+            }
+            .setNegativeButton("cancelar") { _, _ ->
+
+            }
+            .show()
     }
 
     private fun createProduct() {
